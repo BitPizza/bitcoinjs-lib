17ed2c589944c512ce9b7806d12726b19b4ccd6f
diff <bitkey.tech@gmail.com>
2018-10-18 14:50:19 +0800

    bitcoinMessage_sign

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const script = require('./script')
 const SafeBuffer = require('safe-buffer').Buffer
+const secp256k1 = require('secp256k1')
+const varuint = require('varuint-bitcoin')
 
 module.exports = {
   Block: require('./block'),
@@ -16,5 +18,7 @@ module.exports = {
   opcodes: require('bitcoin-ops'),
   payments: require('./payments'),
   SafeBuffer: SafeBuffer,
+  varuint: varuint,
+  secp256k1: secp256k1,
   script: script
 }
